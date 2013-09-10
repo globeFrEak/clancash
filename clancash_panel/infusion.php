469de6f409b35723b7ac553a51f278dc012acfd9
Sonic <angelusdiabolus@online.de>
2013-09-10 20:15:15 +0200

    Update infusion.php
    Änderungen wurden rückgängig gemacht

--- clancash_panel/infusion.php
+++ clancash_panel/infusion.php
@@ -103,9 +103,7 @@ $inf_droptable[3] = DB_CCP_KATEGORIEN;
 $inf_droptable[4] = DB_CCP_KONTEN;
 $inf_droptable[5] = DB_CCP_SETTINGS;
 
-$inf_insertdbrow[1] = DB_PANELS." SET panel_name='".$inf_title."', panel_filename='".$inf_folder."', panel_side=1, panel_order='4', panel_type='file', panel_access='101', panel_display='0', panel_status='1' ";
-
-$inf_deldbrow[1] = DB_PANELS." WHERE panel_filename='".$inf_folder."'";
+$inf_insertdbrow[1] = "";
 
 $inf_adminpanel[1] = array(
     "title" => $locale['ccp000'], 
@@ -113,9 +111,4 @@ $inf_adminpanel[1] = array(
     "panel" => "ccp_settings_panel.php", 
     "rights" => "CCP" 
 );
-$inf_sitelink[1] = array(
-	"title" => $locale['install_003'],
-	"url" => "urlaub.php",
-	"visibility" => "101" // 0 - Guest / 101 - Member / 102 - Admin / 103 - Super Admin.
-)
 ?>
