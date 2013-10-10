11eb4430b51ab49c96fecd44c5c441f189dbd694
globeFrEak <globefreak@web.de>
2013-10-10 17:30:28 +0200

    copyright gekürzt

--- clancash_panel/ccp_copyright.php
+++ clancash_panel/ccp_copyright.php
@@ -1,24 +1,29 @@
 <?PHP
-/*---------------------------------------------------+
-| PHP-Fusion 7 Content Management System
-+----------------------------------------------------+
-| Copyright © 2002 - 2013 Nick Jones
-| http://www.php-fusion.co.uk/
-|----------------------------------------------------+
-| Infusion: ClanCash
-| Filename: ccp_admin_panel.php
-| Author: 
-| RedDragon(v6) 	http://www.efc-funclan.de
-| globeFrEak (v7) 	http://www.cwclan.de
-| Sonic (v7.02)		http://www.germanys-united-legends.de
-+----------------------------------------------------+
-| This program is released as free software under the
-| Affero GPL license. You can redistribute it and/or
-| modify it under the terms of this license which you
-| can read by viewing the included agpl.txt or online
-| at www.gnu.org/licenses/agpl.html. Removal of this
-| copyright header is strictly prohibited without
-| written permission from the original author(s).
-+----------------------------------------------------*/
-echo "<br><center><span style=color:black;font-size:12px>&copy;2010 <a href='http://www.efc-funclan.de'>RedDragon</a> <br>portiert von <a href='http://www.cwclan.de'>globeFrEak</a> 2011 auf PHP-FUSION (v7) <br> &uuml;berarbeitet von <a href='http://www.germanys-united-legends.de'>Sonic</a> 2013 f&uuml;r PHP-FUSION (v7.02) <br> Clancash Panel V".$locale['ccp002']."</span></center>";
+
+/* ---------------------------------------------------+
+  | PHP-Fusion 7 Content Management System
+  +----------------------------------------------------+
+  | Copyright © 2002 - 2013 Nick Jones
+  | http://www.php-fusion.co.uk/
+  |----------------------------------------------------+
+  | Infusion: ClanCash
+  | Filename: ccp_admin_panel.php
+  | Author:
+  | RedDragon(v6) 	http://www.efc-funclan.de
+  | globeFrEak (v7) 	http://www.cwclan.de
+  | Sonic (v7.02)		http://www.germanys-united-legends.de
+  +----------------------------------------------------+
+  | This program is released as free software under the
+  | Affero GPL license. You can redistribute it and/or
+  | modify it under the terms of this license which you
+  | can read by viewing the included agpl.txt or online
+  | at www.gnu.org/licenses/agpl.html. Removal of this
+  | copyright header is strictly prohibited without
+  | written permission from the original author(s).
+  +---------------------------------------------------- */
+echo "<br><center>
+    <span style=color:black;font-size:12px>&copy;2010 
+    <a href='http://www.efc-funclan.de'>RedDragon</a> <br>     
+    &uuml;berarbeitet von <a href='http://www.germanys-united-legends.de'>Sonic</a> und <a href='http://www.cwclan.de'>globeFrEak</a> 2013 f&uuml;r PHP-FUSION (v7.02) <br> 
+    Clancash Panel V" . $locale['ccp002'] . "</span></center>";
 ?>
