218293e46e4e46ba31b97fc2428f7b2d64eb7853
globeFrEak <globefreak@web.de>
2014-01-25 19:52:22 +0100

    Copyright

--- clancash_panel/ccp_filter.php
+++ clancash_panel/ccp_filter.php
@@ -1,24 +1,23 @@
 <?php
 /*--------------------------------------------------------------+
- | PHP-Fusion 7 Content Management System             			|
+ | PHP-Fusion 7 Content Management System             		|
  +--------------------------------------------------------------+
- | Copyright © 2002 - 2013 Nick Jones                 			|
- | http://www.php-fusion.co.uk/                       			|
+ | Copyright © 2002 - 2013 Nick Jones                 		|
+ | http://www.php-fusion.co.uk/                       		|
  +--------------------------------------------------------------+
- | Infusion: ClanCash                                 			|
- | Filename: ccp_admin_panel.php                      			|
- | Author:                                            			|
- | RedDragon(v6) 	    http://www.efc-funclan.de      			|
- | globeFrEak (v7) 		http://www.cwclan.de           			|
+ | Infusion: ClanCash                                 		|
+ | Author:                                            		|
+ | RedDragon(v6) 	    http://www.efc-funclan.de      	|
+ | globeFrEak (v7) 		http://www.cwclan.de           	|
  | GUL-Sonic (v7.02)	http://www.germanys-united-legends.de 	|
  +--------------------------------------------------------------+
- | This program is released as free software under the			|
- | Affero GPL license. You can redistribute it and/or			|
- | modify it under the terms of this license which you			|
- | can read by viewing the included agpl.txt or online			|
- | at www.gnu.org/licenses/agpl.html. Removal of this			|
- | copyright header is strictly prohibited without				|
- | written permission from the original author(s).				|
+ | This program is released as free software under the		|
+ | Affero GPL license. You can redistribute it and/or		|
+ | modify it under the terms of this license which you		|
+ | can read by viewing the included agpl.txt or online		|
+ | at www.gnu.org/licenses/agpl.html. Removal of this		|
+ | copyright header is strictly prohibited without		|
+ | written permission from the original author(s).		|
  +--------------------------------------------------------------*/
 if (!defined("IN_FUSION") || !IN_FUSION) die("Access denied!"); 
 
