2cd345a47eaaa4d96fecf336bfa5d0c119c5817c
Philipp <globefreak@web.de>
2014-04-27 19:04:04 +0200

    versions Icons
    neue Icons für die Versionkontrolle

--- clancash_panel/ccp_settings_panel.php
+++ clancash_panel/ccp_settings_panel.php
@@ -178,7 +178,7 @@ if (version_compare($new_version, $ccp_version, '<=') AND $new_version > 0) {
     $ausgabe = "
 	  <table cellpadding='0' cellspacing='1'>
 	  <tr>
-	  <td><img src='" . INFUSIONS . "clancash_panel/images/version.gif' alt='up to date' /></td>
+	  <td><img src='" . INFUSIONS . "clancash_panel/images/version.png' alt='up to date' /></td>
 	  <td><span class='ccp_update'>" . $locale['ccp303'] . ": " . $ccp_version . "</span></td>
 	  </tr>
 	  </table>";
@@ -187,7 +187,7 @@ if (version_compare($new_version, $ccp_version, '<=') AND $new_version > 0) {
         $ausgabe = "
 	<table cellpadding='0' cellspacing='1'>
 	<tr>
-	<td><img src='" . INFUSIONS . "clancash_panel/images/version_old.gif' alt='old version' /></td>
+	<td><img src='" . INFUSIONS . "clancash_panel/images/version_old.png' alt='old version' /></td>
 	<td><span class='ccp_negative'>" . $locale['ccp302'] . ": " . $ccp_version . "</span><br />
 	<span class='ccp_positive'>" . $locale['ccp301'] . ": " . $new_version . "</span><br />
 	<span style='font-weight: bold;'>" . $locale['ccp304'] . ": </span><a href='http://globefreak.github.io/clancash/' target='_blank' title='" . $locale['ccp312'] . "'><span style='font-weight: bold;'>" . $locale['ccp312'] . "</span></a></td>
